cfd5ef48cf425a1713fd430f395fbeb1b214bca2
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2022-04-11 17:13:43 +0200

    Add project files.

---
+++ RestaurantAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherForecastService _service;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpPost("generate")]
+        public ActionResult<IEnumerable<WeatherForecast>> Generate([FromQuery] int count,
+            [FromBody] TemperatureRequest request)
+        {
+            if (count < 0 || request.Max < request.Max)
+            {
+                return BadRequest();
+            }
+
+            var result = _service.Get(count, request.Min, request.Max);
+            return Ok(result);
+        }
+    }
+}
